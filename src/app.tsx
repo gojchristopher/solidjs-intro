3e25e70ed0842b96470da9aec8daf8a3e3ba8e8c
calvo-jp <calvojp92@gmail.com>
2022-09-04 13:22:08 +0800

    removed suspense

--- src/app.tsx
+++ src/app.tsx
@@ -1,32 +1,25 @@
 import {Route, Router, Routes} from '@solidjs/router';
-import {ErrorBoundary, lazy, Suspense} from 'solid-js';
+import {ErrorBoundary} from 'solid-js';
 import ErrorFallback from '~/components/error-fallback';
-
-const Post = lazy(() => import('~/pages/post'));
-const Posts = lazy(() => import('~/pages/posts'));
-const Login = lazy(() => import('~/pages/login'));
-const Landing = lazy(() => import('~/pages/landing'));
-const Layout = lazy(() => import('~/components/layout'));
+import Layout from './components/layout';
+import Landing from './pages/landing';
+import Login from './pages/login';
+import Post from './pages/post';
+import Posts from './pages/posts';
 
 export default function App() {
   return (
-    <Suspense fallback={Loader}>
-      <Router>
-        <ErrorBoundary fallback={ErrorFallback}>
-          <Routes>
-            <Route path="/" component={Layout}>
-              <Route path="/" component={Landing} />
-              <Route path="/login" component={Login} />
-              <Route path="/posts" component={Posts} />
-              <Route path="/posts/:id" component={Post} />
-            </Route>
-          </Routes>
-        </ErrorBoundary>
-      </Router>
-    </Suspense>
+    <Router>
+      <ErrorBoundary fallback={ErrorFallback}>
+        <Routes>
+          <Route path="" component={Layout}>
+            <Route path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/posts/:id" component={Post} />
+          </Route>
+        </Routes>
+      </ErrorBoundary>
+    </Router>
   );
-}
-
-function Loader() {
-  return <div class="p-2 text-sm text-gray-500">Loading...</div>;
 }
