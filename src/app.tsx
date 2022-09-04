be460af34a9f42945b4819cd480a3c81d9a0fcf6
calvo-jp <calvojp92@gmail.com>
2022-09-04 13:10:11 +0800

    lazy load all components

--- src/app.tsx
+++ src/app.tsx
@@ -1,30 +1,32 @@
 import {Route, Router, Routes} from '@solidjs/router';
-import {ErrorBoundary} from 'solid-js';
-import {Toaster} from 'solid-toast';
-import Navbar from '~/components/navbar';
-import Landing from '~/pages/landing';
-import ErrorFallback from './components/error-fallback';
-import Login from './pages/login';
-import Post from './pages/post';
-import Posts from './pages/posts';
+import {ErrorBoundary, lazy, Suspense} from 'solid-js';
+import ErrorFallback from '~/components/error-fallback';
+
+const Post = lazy(() => import('~/pages/post'));
+const Posts = lazy(() => import('~/pages/posts'));
+const Login = lazy(() => import('~/pages/login'));
+const Landing = lazy(() => import('~/pages/landing'));
+const Layout = lazy(() => import('~/components/layout'));
 
 export default function App() {
   return (
-    <Router>
-      <ErrorBoundary fallback={ErrorFallback}>
-        <Navbar />
-
-        <main class="max-w-[600px] mx-auto p-4 md:p-8 lg:p-12">
+    <Suspense fallback={Loader}>
+      <Router>
+        <ErrorBoundary fallback={ErrorFallback}>
           <Routes>
-            <Route path="/" component={Landing} />
-            <Route path="/login" component={Login} />
-            <Route path="/posts" component={Posts} />
-            <Route path="/posts/:id" component={Post} />
+            <Route path="/" component={Layout}>
+              <Route path="/" component={Landing} />
+              <Route path="/login" component={Login} />
+              <Route path="/posts" component={Posts} />
+              <Route path="/posts/:id" component={Post} />
+            </Route>
           </Routes>
-
-          <Toaster />
-        </main>
-      </ErrorBoundary>
-    </Router>
+        </ErrorBoundary>
+      </Router>
+    </Suspense>
   );
+}
+
+function Loader() {
+  return <div class="p-2 text-sm text-gray-500">Loading...</div>;
 }
