fa7ec495effb4e4db53804ff05220c458e825ff5
calvo-jp <calvojp92@gmail.com>
2022-09-04 16:29:47 +0800

    removed duplicate router

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import {Route, Router, Routes} from '@solidjs/router';
+import {Route, Routes} from '@solidjs/router';
 import {ErrorBoundary} from 'solid-js';
 import ErrorFallback from './components/error-fallback';
 import Layout from './components/layout';
@@ -9,17 +9,15 @@ import Posts from './pages/posts';
 
 export default function App() {
   return (
-    <Router>
-      <ErrorBoundary fallback={ErrorFallback}>
-        <Routes>
-          <Route path="" component={Layout}>
-            <Route path="/" component={Landing} />
-            <Route path="/login" component={Login} />
-            <Route path="/posts" component={Posts} />
-            <Route path="/posts/:id" component={Post} />
-          </Route>
-        </Routes>
-      </ErrorBoundary>
-    </Router>
+    <ErrorBoundary fallback={ErrorFallback}>
+      <Routes>
+        <Route path="" component={Layout}>
+          <Route path="/" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/posts/:id" component={Post} />
+        </Route>
+      </Routes>
+    </ErrorBoundary>
   );
 }
