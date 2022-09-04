fbf652ab1923e94073e064bef508fe9b6d39e293
calvo-jp <calvojp92@gmail.com>
2022-09-04 11:18:45 +0800

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,51 @@
-import {useNavigate} from '@solidjs/router';
+import {Link, useLocation, useNavigate} from '@solidjs/router';
+import {Show} from 'solid-js';
+import {authState, setAuthState} from '~/hooks/use-auth-state';
 
 export default function Navbar() {
-  const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <header class="border-b border-gray-300">
-      <div class="px-8 py-4 max-w-[1100px] mx-auto flex items-center justify-between">
-        <h2 class="text-2xl font-semibold">Logo</h2>
+      <div class="px-4 md:px-6 lg:px-8 py-4 max-w-[1100px] mx-auto flex items-center justify-between">
+        <Link href="/">
+          <h2 class="text-2xl font-semibold">Logo</h2>
+        </Link>
 
         <nav>
           <ul>
-            <li>
-              <button onClick={() => navigate('/login')}>Logout</button>
-            </li>
+            <Show when={authState().loggedIn}>
+              <li>
+                <Logout />
+              </li>
+            </Show>
+
+            <Show when={!authState().loggedIn && location.pathname === '/'}>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+            </Show>
           </ul>
         </nav>
       </div>
     </header>
+  );
+}
+
+function Logout() {
+  const navigate = useNavigate();
+
+  return (
+    <button
+      onClick={() => {
+        navigate('/login');
+        setAuthState({
+          user: null,
+          loggedIn: false,
+        });
+      }}
+    >
+      Logout
+    </button>
   );
 }
