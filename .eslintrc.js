90f79028dd9cc6b9a83460b9492d90ff484ac84f
calvo-jp <calvojp92@gmail.com>
2022-09-04 19:19:24 +0800

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-shadow': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    'prefer-template': 'warn',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/no-duplicate-imports': 'error',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'next.config.js',
+    'tailwind.config.js',
+    'postcss.config.js',
+    'dist',
+  ],
+};
