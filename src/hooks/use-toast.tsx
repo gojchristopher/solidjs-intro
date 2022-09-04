be6e53bdc51b20f8a3d2d3410ac41d2f70b43496
calvo-jp <calvojp92@gmail.com>
2022-09-04 11:34:13 +0800

    add toast variant danger

--- src/hooks/use-toast.tsx
+++ src/hooks/use-toast.tsx
@@ -2,23 +2,54 @@ import toast from 'solid-toast';
 import CheckCircleSolidIcon from '~/components/icons/check-circle-solid';
 import CloseSolidIcon from '~/components/icons/close-icon-solid';
 
-type ToastVariant = 'error' | 'success';
+type ToastVariant = 'danger' | 'success';
 
 export default function useToast() {
-  return (message: string, _variant: ToastVariant = 'success') => {
+  return (message: string, variant: ToastVariant = 'success') => {
+    toast.dismiss();
     toast.custom(
-      <div class="bg-teal-100 shadow-md p-4 w-[350px] max-w-[450px] flex items- gap-2">
-        <CheckCircleSolidIcon class="fill-teal-500 w-5 h-5 grow-0 shrink-0" />
+      ({id}) => (
+        <div
+          class="shadow-md p-4 w-[350px] max-w-[450px] flex gap-2"
+          classList={{
+            'bg-teal-100': variant === 'success',
+            'bg-rose-100': variant === 'danger',
+          }}
+        >
+          <CheckCircleSolidIcon
+            class="w-5 h-5 grow-0 shrink-0"
+            classList={{
+              'fill-teal-400': variant === 'success',
+              'fill-rose-400': variant === 'danger',
+            }}
+          />
 
-        <p class="grow text-teal-700 leading-5">{message}</p>
+          <p
+            class="grow leading-5"
+            classList={{
+              'text-teal-600': variant === 'success',
+              'text-rose-600': variant === 'danger',
+            }}
+          >
+            {message}
+          </p>
 
-        <button>
-          <CloseSolidIcon class="fill-teal-500 w-5 h-5 grow-0 shrink-0" />
-        </button>
-      </div>,
+          <button>
+            <CloseSolidIcon
+              class="w-5 h-5 grow-0 shrink-0"
+              classList={{
+                'fill-teal-400': variant === 'success',
+                'fill-rose-400': variant === 'danger',
+              }}
+              onClick={() => toast.dismiss(id)}
+            />
+          </button>
+        </div>
+      ),
       {
         position: 'top-center',
-        unmountDelay: 3000,
+        unmountDelay: 0,
+        duration: 3000,
       },
     );
   };
