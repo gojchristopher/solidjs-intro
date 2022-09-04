ce425ef076f84256c92282c26e7974ca880f06d3
calvo-jp <calvojp92@gmail.com>
2022-09-04 13:11:50 +0800

    adjust gap

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -6,7 +6,7 @@ export default function PostList(props: {data: TPost[]}) {
   const [local] = splitProps(props, ['data']);
 
   return (
-    <div class="flex flex-col gap-4">
+    <div class="flex flex-col gap-3 md:gap-4">
       <Index each={local.data}>
         {(post) => {
           return <Post data={post()} />;
